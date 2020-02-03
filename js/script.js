1c4f5e91083ecd02b4b7f4f12c86c774106bfdb4
Akmal <akmalnba@gmail.com>
2020-02-04 02:53:26 +0500

    asdf

---
+++ js/script.js
@@ -0,0 +1,34 @@
+const ready = (callback) => {
+    window.addEventListener('DOMContentLoaded', (e) => {
+        callback(e);
+    })
+}
+
+ready(() => {
+    const MenuClassToggler = () => {
+        const btn = document.querySelector('nav .toggle-btn input');
+        const menu = document.querySelector('nav .nav-menu');
+        const navcontacts = document.querySelector('nav .nav-contacts');
+
+        btn.addEventListener('click', () => {
+            menu.classList.toggle('active');
+            navcontacts.classList.toggle('active');
+            document.querySelector('body').classList.toggle('overhidden');
+        });
+    }
+
+    MenuClassToggler();
+});
+
+
+// const fontAwesomeFreeObserver = new FontFaceObserver('Font Awesome 5 Free');
+// const fontAwesomeBrandsObserver = new FontFaceObserver('Font Awesome 5 Brands');
+// const gilroyObserver = new FontFaceObserver('Gilroy');
+
+// Promise.all([
+//     fontAwesomeFreeObserver.load(),
+//     fontAwesomeBrandsObserver.load(),
+//     gilroyObserver.load()
+// ]).then(() => {
+//     document.querySelector('html').classList.add('fonts-loaded');
+// });
